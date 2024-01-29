3fabc38119d78ba5e21e1961c2887a423a7a3536
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-01-29 20:03:29 +0200

    Завдання 2 Перевірка на позитивну затримку

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,26 +1,39 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import debounce from "lodash.debounce";
 // Знаходимо в розмітці  форму, інпут де вказується затримка та один з елементів вибору
 const notificationForm = {
   submitForm: document.querySelector('.form'),
-  delayInput: document.querySelector('input[name="delay"'),
+  delayInput: document.querySelector('input[name="delay"]'),
   stateFulFilled: document.querySelector('input[value="fulfilled"]'),
 };
-console.dir(notificationForm.submitForm);
+const debouncedInputValue=debounce(ondelayInputInput,1000)
 // Додаємо прослуховувач подій на форму
 notificationForm.submitForm.addEventListener('submit', onFormSubmit);
+// Додаємо прослуховувач подій на поле для вводу затримки часу
+notificationForm.delayInput.addEventListener('input', debouncedInputValue);
+//Функція перевірки чи додатня величиниа затримки повідомлення
+function ondelayInputInput(event) {
+  let inputValue = event.target.value;
+  if (inputValue < 0) {
+    showNotificationForNegativeValueOfTimeDelay();
+    notificationForm.submitForm.reset();
+     }
+  
+};
 // вказуємо що робити при "сабміті" форми
 function onFormSubmit(event) {
-    event.preventDefault();
-   const delay = notificationForm.delayInput.value;
-   let state =notificationForm.stateFulFilled.checked
-    ? 'resolve'
-    : 'reject';
+  event.preventDefault();
+  
+    const delay = notificationForm.delayInput.value;
+    let state = notificationForm.stateFulFilled.checked
+      ? 'resolve'
+      : 'reject';
     executePromise(state, delay)
-    .then(showFulfilledNotification)
-    .catch(showRejectNotification);
-  setTimeout(() => { notificationForm.submitForm.reset(); }, 1000);
-};
+      .then(showFulfilledNotification)
+      .catch(showRejectNotification);
+    notificationForm.submitForm.reset();
+ };
 // Створюємо функцію яка показує повідомлення при вдалому виконанні промісу
 function showFulfilledNotification(delay) {
   setTimeout(() => {
@@ -45,6 +58,15 @@ function showRejectNotification(delay) {
     });
   }, delay);
 };
+// Створюємо функцію яка  показує попередження про введення відємного відліку часу
+function showNotificationForNegativeValueOfTimeDelay() {
+  iziToast.error({
+      timeout: 3000,
+      title: '',
+      message: `Please enter positive value for time delay`,
+      position: 'topCenter',
+    });
+}
 // Створюємо проміс
 function executePromise(state, delay) {
     return new Promise((resolve, reject) => { if (state === 'resolve') { resolve(delay); } else { reject(delay); }; });
